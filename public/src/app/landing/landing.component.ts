5de51dae5d723f11def9f6c24ae0e363856f9a8d
Kenny Jiang <jiang.kennyy@gmail.com>
2019-09-09 19:26:35 -0700

    No node modules

---
+++ public/src/app/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+// declare var Navbar: any;
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
