13899da25fc5b7f0819d6ace620a1924817f0ae0
Kenny Jiang <jiang.kennyy@gmail.com>
2019-09-15 17:48:20 -0700

    :(

---
+++ public/src/app/key.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeyService {
+  public apiKey= 'Bearer k_FCAa7u_MafN-xCdxbhz2v2v-v5r-wjQH-giUcR-6BrcmZWbErKsDbnNWtUmBlpWgDkYhDy55VgHiQWWkfzNSzcESMpjXzQhgvq4GwODte1beWPjAIZJ3h2Me56XXYx'
+
+  constructor() { }
+
+}
