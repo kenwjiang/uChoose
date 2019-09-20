f9f05ad53fea339758c740ee3814eb8f048f5c2a
Kenny Jiang <jiang.kennyy@gmail.com>
2019-09-19 18:50:27 -0700

    zxc

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 var path = require('path');
 app.use(express.static( __dirname + '/public/dist/public' ));
 
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/');
+
+app.get("/portfolio", (req, res)=> {
+  res.render('index');
+})
+
 app.all("*", (req,res,next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
 });
