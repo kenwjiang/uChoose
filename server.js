b43847d6b21b0dcb355a3278831e09219d6c3ad6
Kenny Jiang <jiang.kennyy@gmail.com>
2019-09-19 18:46:04 -0700

    roll back

--- server.js
+++ server.js
@@ -8,20 +8,10 @@ app.use(bodyParser.json());
 var path = require('path');
 app.use(express.static( __dirname + '/public/dist/public' ));
 
-app.use(express.static(__dirname + "/static"));
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/');
-
-
-app.get("/portfolio", (req, res)=>{
-    res.render('index');
-})
-app.all("", (req,res,next) => {
+app.all("*", (req,res,next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
 });
-app.all("/wheel", (req,res,next) => {
-    res.sendFile(path.resolve("./public/dist/public/index.html"))
-});
+
 
 app.listen(8000, function(){
     console.log('Currently connected to port: 8000');
